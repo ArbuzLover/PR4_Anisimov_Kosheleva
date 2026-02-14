177a34e5d650bafa2e50855495f4c4842ace6564
ArbuzLover <Yan201901@yandex.ru>
2026-02-14 14:23:41 +0300

    переделал первую страницу

--- Практика4_Анисимов&Кошелева/Pages/Page1.xaml.cs
+++ Практика4_Анисимов&Кошелева/Pages/Page1.xaml.cs
@@ -34,17 +34,8 @@ namespace Практика4_Анисимов_Кошелева.Pages
                 double z = Convert.ToDouble(Z.Text);
 
 
-                double firstPart = Math.Pow(2, -x);
-                double fourthRootY = Math.Pow(Math.Abs(y), 1.0 / 4.0);
-                double expXminus1 = Math.Exp(x - 1);
-                double sinZ = Math.Sin(z);
-                double divisionResult = expXminus1 / sinZ;
-                double cubeRoot = Math.Pow(divisionResult, 1.0 / 3.0);
-                double product = fourthRootY * cubeRoot;
-                double sum = x + product;
-                double sqrtPart = Math.Sqrt(sum);
-                double result = firstPart * sqrtPart;
-                Answer.Text = $"{result}";
+                
+                Answer.Text = $"{Math.Pow(2, -x) * Math.Sqrt(x + Math.Pow(Math.Abs(y), 1.0 / 4.0) * Math.Pow(Math.Exp(x - 1) / Math.Sin(z), 1.0 / 3.0))}";
             }
             else MessageBox.Show("Введите все значения");
         }
