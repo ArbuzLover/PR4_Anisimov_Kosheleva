ec7b620edf434fd7151dce04fa26206e9fef6d3b
234918 <234918@KB37-212-C03>
2026-03-11 14:19:37 +0300

    Создал пустые методы

---
+++ TestPage1/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace TestPage1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TestPage2/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace TestPage2
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
